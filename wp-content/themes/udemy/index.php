d706c9700dffcf36d0898a637ceaeec18c8cdab6
Alek Novkovski <alek@greatcode2.com>
2022-10-20 01:36:06 +0200

    Add wp_head and wp_footer to theme's index.php

--- wp-content/themes/udemy/index.php
+++ wp-content/themes/udemy/index.php
@@ -1,20 +1,19 @@
 <?php
-$charset = get_bloginfo( 'charset');
+$charset = get_bloginfo( 'charset' );
 ?>
 
-<!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?= $charset ?>">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+    <!doctype html>
+    <html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?= $charset ?>">
+		<?php wp_head(); ?>
+    </head>
+    <body>
     <p>Helooo World!</p>
-</body>
-</html>
+
+	<?php wp_footer(); ?>
+    </body>
+    </html>
 
 <?php
 
