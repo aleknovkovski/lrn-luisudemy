fe014ae001e62a45a89a5a2ec149bf8c81d907b1
Alek Novkovski <alek@greatcode2.com>
2022-10-20 00:19:46 +0200

    fix git config file, semicolon was missing and causing errors

--- wp-config.php
+++ wp-config.php
@@ -74,7 +74,7 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 define( 'WP_DEBUG', true );
-define('WP_DISABLE_FATAL_ERROR_HANDLER', true)
+define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 
 /* Add any custom values between this line and the "stop editing" line. */
 
