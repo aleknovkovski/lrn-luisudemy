44bea314474832b55636a7e8ff5945582ac6f522
Alek Novkovski <alek@greatcode2.com>
2022-10-20 01:22:01 +0200

    Replace charset with dynamic wordpress value

--- wp-content/themes/udemy/index.php
+++ wp-content/themes/udemy/index.php
@@ -1,11 +1,11 @@
 <?php
-
+$charset = get_bloginfo( 'charset');
 ?>
 
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?= $charset ?>">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
