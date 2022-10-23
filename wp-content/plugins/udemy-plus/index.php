83f21c4ad8a6aad557b03786d5e0f3253670c314
Alek Novkovski <alek@greatcode2.com>
2022-10-23 22:49:42 +0200

    Register the Udemy Plus plugin

---
+++ wp-content/plugins/udemy-plus/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       Udemy Plus
+ * Plugin URI:        https://udemy.com
+ * Description:       A plugin for adding blocks to the Udemy theme.
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            Udemy
+ * Author URI:        https://udemy.com
+ * Text Domain:       udemy-plus
+ * Domain Path:       /languages
+ */
