40b880c44c1186c61d1a2cda573881e4a79f2272
Alek Novkovski <alek@greatcode2.com>
2022-10-24 16:55:37 +0200

    Assign svg icon to search-form block

--- wp-content/plugins/udemy-plus/src/blocks/search-form/index.js
+++ wp-content/plugins/udemy-plus/src/blocks/search-form/index.js
@@ -1,8 +1,10 @@
 import { registerBlockType } from "@wordpress/blocks";
 import block from "./block.json";
 import './main.css'
+import icons from "../../icons";
 
 registerBlockType(block.name, {
+    icon: icons.primary,
     edit() {
         return <p>Search Form</p>;
     },
