add85fc1bdae7fc383548184a401477cda1d21a3
Alek Novkovski <alek@greatcode2.com>
2022-10-24 17:13:28 +0200

    Add the static search-form html in the block's edit fn

--- wp-content/plugins/udemy-plus/src/blocks/search-form/index.js
+++ wp-content/plugins/udemy-plus/src/blocks/search-form/index.js
@@ -6,6 +6,16 @@ import icons from "../../icons";
 registerBlockType(block.name, {
     icon: icons.primary,
     edit() {
-        return <p>Search Form</p>;
+        return (
+            <div className="wp-block-udemy-plus-search-form">
+                <h1>Search: Your search term here</h1>
+                <form>
+                    <input type="text" placeholder="Search"/>
+                    <div className="btn-wrapper">
+                        <button type="submit">Search</button>
+                    </div>
+                </form>
+            </div>
+        );
     },
 });
