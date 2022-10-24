6a39f7dab25be1f1c31018077daec0dc13b09ef5
Alek Novkovski <alek@greatcode2.com>
2022-10-24 05:08:21 +0200

    Refactor edit, wrap RichText w div

--- wp-content/plugins/udemy-plus/src/index.js
+++ wp-content/plugins/udemy-plus/src/index.js
@@ -1,4 +1,4 @@
-import { registerBlockType } from '@wordpress/blocks'
+import {registerBlockType} from '@wordpress/blocks'
 import block from './block.json'
 import {RichText, useBlockProps} from '@wordpress/block-editor'
 import {__} from '@wordpress/i18n'
@@ -6,31 +6,31 @@ import './main.css'
 
 registerBlockType(block.name, {
     edit({attributes, setAttributes}) {
-        const { content } = attributes
+        const {content} = attributes
         const blockProps = useBlockProps()
         console.dir(attributes)
 
-        return <RichText
-            {...blockProps}
-            tagName="h2"
-            placeholder={__('Enter Heading', 'udemy-plus')}
-            value={content}
-            onChange={(newValue, oldValue) => {
-                setAttributes({ content: newValue})
-            }
-            }
-        />
-    },
-    save({attributes}) {
-        const { content } = attributes
-        const blockProps = useBlockProps.save()
+        return <div {...blockProps}>
+            <RichText
+                className="fancy-header"
+                tagName="h2"
+                placeholder={__('Enter Heading', 'udemy-plus')}
+                value={attributes.content}
+                onChange={(newValue, oldValue) => {
+                    setAttributes({content: newValue})
+                }}
+            />
+        </div>
+    }, save({attributes}) {
+        const {content} = attributes
+        const blockProps = useBlockProps.save({
+            className: 'fancy-header'
+        })
 
-        return (
-            <RichText.Content
+        return (<RichText.Content
                 {...blockProps}
                 tagName="h2"
                 value={content}
-            />
-        )
+            />)
     }
 })
