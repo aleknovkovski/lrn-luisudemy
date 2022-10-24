d99c725413a4d48c7f684cdb412b42463a44d80d
Alek Novkovski <alek@greatcode2.com>
2022-10-24 14:48:09 +0200

    Refactor block organization and registration to allow adding multiple blocks

--- wp-content/plugins/udemy-plus/includes/register-blocks.php
+++ wp-content/plugins/udemy-plus/includes/register-blocks.php
@@ -1,7 +1,12 @@
 <?php
 
-function up_register_blocks(){
-	register_block_type(
-		UP_PLUGIN_DIR . 'build/block.json'
-	);
+function up_register_blocks() {
+	$blocks = [
+		['name' => 'fancy-header']
+	];
+	foreach($blocks as $block) {
+		register_block_type(
+			UP_PLUGIN_DIR . 'build/blocks/' . $block['name']
+		);
+	}
 }
