ad62371ff6999b70de3ecfa01657e909a7cf8db5
Alek Novkovski <alek@greatcode2.com>
2022-10-24 05:44:53 +0200

    Add color palette with 2-way-bind on underline_color

--- wp-content/plugins/udemy-plus/src/index.js
+++ wp-content/plugins/udemy-plus/src/index.js
@@ -1,19 +1,28 @@
 import {registerBlockType} from '@wordpress/blocks'
 import block from './block.json'
 import {RichText, useBlockProps, InspectorControls} from '@wordpress/block-editor'
-import {PanelBody} from '@wordpress/components'
+import {PanelBody, ColorPalette} from '@wordpress/components'
 import {__} from '@wordpress/i18n'
 import './main.css'
 
 registerBlockType(block.name, {
     edit({attributes, setAttributes}) {
-        const {content} = attributes
+        const {content, underline_color} = attributes
         const blockProps = useBlockProps()
 
         return (<>
             <InspectorControls>
                 <PanelBody title={__('Colors', 'udemy-plus')}>
-                    Dummy Content
+                    <ColorPalette
+                        colors={[
+                            { name: 'Red', color: '#f87171'},
+                            { name: 'Indigo', color: '#818cf8'}
+                        ]}
+                        value={underline_color}
+                        onChange={newVal => {
+                            setAttributes({ 'underline_color': newVal })
+                        }}
+                    />
                 </PanelBody>
             </InspectorControls>
             <div {...blockProps}>
