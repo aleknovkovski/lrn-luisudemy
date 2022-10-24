7f892f8ad6de29c8cdca821ed1345ffec6f16901
Alek Novkovski <alek@greatcode2.com>
2022-10-24 03:09:36 +0200

    Return a simple RichText component with the block

--- wp-content/plugins/udemy-plus/src/index.js
+++ wp-content/plugins/udemy-plus/src/index.js
@@ -1,8 +1,13 @@
 import { registerBlockType } from '@wordpress/blocks'
 import block from './block.json'
+import {RichText} from '@wordpress/block-editor'
+import {__} from '@wordpress/i18n'
 
 registerBlockType(block.name, {
     edit() {
-        return <p>Hello world</p>
+        return <RichText
+            tagName="h2"
+            placeholder={__('Enter Heading', 'udemy-plus')}
+        />
     }
 })
