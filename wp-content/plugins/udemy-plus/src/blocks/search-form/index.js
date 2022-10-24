c4e5b17b25604e7763f165a374bab73a72e6f03e
Alek Novkovski <alek@greatcode2.com>
2022-10-24 16:11:12 +0200

    Import main.css, add css inside for testing if it loads in editor

--- wp-content/plugins/udemy-plus/src/blocks/search-form/index.js
+++ wp-content/plugins/udemy-plus/src/blocks/search-form/index.js
@@ -1,5 +1,7 @@
 import { registerBlockType } from "@wordpress/blocks";
 import block from "./block.json";
+import './main.css'
+
 registerBlockType(block.name, {
     edit() {
         return <p>Search Form</p>;
