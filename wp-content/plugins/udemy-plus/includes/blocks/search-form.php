82feb56a719dd277bbb6a9f4862ea189fe9e7dc2
Alek Novkovski <alek@greatcode2.com>
2022-10-25 04:23:29 +0200

    Copy editFn html to renderCB; wrap it in output buffers

--- wp-content/plugins/udemy-plus/includes/blocks/search-form.php
+++ wp-content/plugins/udemy-plus/includes/blocks/search-form.php
@@ -1,5 +1,22 @@
 <?php
 
-function up_search_form_render_cb() {
-	return 'Search Form';
+function up_search_form_render_cb(): string {
+	ob_start();
+	?>
+	<div {...blockProps}>
+		<h1>Search: Your search term here</h1>
+		<form>
+			<input type="text" placeholder="Search" />
+			<div className="btn-wrapper">
+				<button type="submit" style={{
+				"background-color": bgColor,
+				color: textColor
+				}}>Search</button>
+			</div>
+		</form>
+	</div>
+	<?php
+	$output = ob_get_contents();
+	ob_end_clean();
+	return $output;
 }
