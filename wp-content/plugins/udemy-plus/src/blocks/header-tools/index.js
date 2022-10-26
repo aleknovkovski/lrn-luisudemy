df1441e3811a4fbdfc10a2cce0ef37f0f0682d7e
Alek Novkovski <alek@greatcode2.com>
2022-10-26 18:49:02 +0200

    Import Checkbox Control

--- wp-content/plugins/udemy-plus/src/blocks/header-tools/index.js
+++ wp-content/plugins/udemy-plus/src/blocks/header-tools/index.js
@@ -1,6 +1,6 @@
 import { registerBlockType } from '@wordpress/blocks';
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, SelectControl } from "@wordpress/components";
+import { PanelBody, SelectControl, CheckboxControl } from "@wordpress/components";
 import { __ } from '@wordpress/i18n';
 import icons from '../../icons.js'
 import './main.css'
