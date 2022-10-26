3db24f3f50ed527efb2d6c01257ef4f4405cc760
Alek Novkovski <alek@greatcode2.com>
2022-10-26 23:40:39 +0200

    Make markup conditional on showAuth value

--- wp-content/plugins/udemy-plus/includes/blocks/header-tools.php
+++ wp-content/plugins/udemy-plus/includes/blocks/header-tools.php
@@ -3,19 +3,19 @@
 function up_header_tools_render_cb($atts) {
 	ob_start(); ?>
 
-	<div class="wp-block-udemy-plus-header-tools">
-        <a class="signin-link open-modal" href="#">
-            <div class="signin-icon">
-                <i class="bi bi-person-circle"></i>
-            </div>
-            <div class="signin-text">
-                <small>Hello, Sign in</small>
-                My Account
-             </div>
-        </a>
-    </div>
-
-	<?php
+	<?php if($atts['showAuth']) { ?>
+        <div class="wp-block-udemy-plus-header-tools">
+            <a class="signin-link open-modal" href="#">
+                <div class="signin-icon">
+                    <i class="bi bi-person-circle"></i>
+                </div>
+                <div class="signin-text">
+                    <small>Hello, Sign in</small>
+                    My Account
+                </div>
+            </a>
+        </div>
+	<?php }
 	$output = ob_get_contents();
 	ob_end_clean();
 	return $output;
