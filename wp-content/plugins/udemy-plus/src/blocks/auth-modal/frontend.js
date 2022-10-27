73f63b97b6b123d075940b19063dcb47e3e669a1
Alek Novkovski <alek@greatcode2.com>
2022-10-27 17:30:10 +0200

    Add event listener to triggers, adds modal-show class on click

--- wp-content/plugins/udemy-plus/src/blocks/auth-modal/frontend.js
+++ wp-content/plugins/udemy-plus/src/blocks/auth-modal/frontend.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const openModalBtn = document.querySelectorAll('.open-modal')
     const modalEl = document.querySelector('.wp-block-udemy-plus-auth-modal')
+
+    openModalBtn.forEach((el)=> {
+        el.addEventListener('click', (event)=> {
+            modalEl.classList.add('modal-show')
+        })
+    })
 });
