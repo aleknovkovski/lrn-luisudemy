78ae8e734a063f9bcbf1563b8b6e166e14f42c78
Alek Novkovski <alek@greatcode2.com>
2022-10-27 23:54:57 +0200

    Get params from request

--- wp-content/plugins/udemy-plus/includes/rest-api/signup.php
+++ wp-content/plugins/udemy-plus/includes/rest-api/signup.php
@@ -1,7 +1,9 @@
 <?php
 
-function up_rest_api_signup_handler() {
+function up_rest_api_signup_handler($request) {
 	$response = ['status' => 1];
+	$params = $request->get_json_params();
+
 	$response['status'] = 2;
 	return $response;
 }
