371678915ed68fe04696920358782b53b2ac0256
Alek Novkovski <alek@greatcode2.com>
2022-10-28 00:26:02 +0200

    Return params, for testing/demo purposes

--- wp-content/plugins/udemy-plus/includes/rest-api/signup.php
+++ wp-content/plugins/udemy-plus/includes/rest-api/signup.php
@@ -4,6 +4,8 @@ function up_rest_api_signup_handler($request) {
 	$response = ['status' => 1];
 	$params = $request->get_json_params();
 
+	return $params;
+
 	$response['status'] = 2;
 	return $response;
 }
