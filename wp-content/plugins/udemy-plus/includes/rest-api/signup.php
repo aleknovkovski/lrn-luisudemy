bd46e1debe6f5b49c13fda65c4dd79bcc358d2ff
Alek Novkovski <alek@greatcode2.com>
2022-10-28 21:42:25 +0200

    Register login event

--- wp-content/plugins/udemy-plus/includes/rest-api/signup.php
+++ wp-content/plugins/udemy-plus/includes/rest-api/signup.php
@@ -41,6 +41,9 @@ function up_rest_api_signup_handler($request) {
 	wp_set_current_user($userID);
 	wp_set_auth_cookie($userID);
 
+	$user = get_user_by('id', $userID);
+	do_action('wp_login', $user->user_login, $user);
+
 	$response['status'] = 2;
 	return $response;
 }
