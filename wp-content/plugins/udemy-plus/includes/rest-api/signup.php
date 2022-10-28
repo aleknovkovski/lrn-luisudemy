ebbe23fd6ade589d082d69e1df487e19edd8a91c
Alek Novkovski <alek@greatcode2.com>
2022-10-28 14:01:49 +0200

    Check for duplicate username, email or wrong email format

--- wp-content/plugins/udemy-plus/includes/rest-api/signup.php
+++ wp-content/plugins/udemy-plus/includes/rest-api/signup.php
@@ -17,6 +17,14 @@ function up_rest_api_signup_handler($request) {
 	$username = sanitize_text_field($params['username']);
 	$password = sanitize_text_field($params['password']);
 
+	if(
+		username_exists($username) ||
+		!is_email($email) ||
+		email_exists($email)
+	) {
+		return $response;
+	}
+
 	$response['status'] = 2;
 	return $response;
 }
