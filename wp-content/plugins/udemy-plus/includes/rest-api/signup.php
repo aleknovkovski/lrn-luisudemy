eccb4553c78247fdd84217333806a3bfac63eeb3
Alek Novkovski <alek@greatcode2.com>
2022-10-28 13:44:01 +0200

    Add validation, are all values submitted; remove test return

--- wp-content/plugins/udemy-plus/includes/rest-api/signup.php
+++ wp-content/plugins/udemy-plus/includes/rest-api/signup.php
@@ -4,7 +4,14 @@ function up_rest_api_signup_handler($request) {
 	$response = ['status' => 1];
 	$params = $request->get_json_params();
 
-	return $params;
+	$valuesNotSet = !isset($params['email'], $params['password'], $params['username']);
+	$emptyEmail = empty($params['email']);
+	$emptyPassword = empty($params['password']);
+	$emptyUsername = empty($params['username']);
+
+	if( $valuesNotSet || $emptyEmail || $emptyPassword || $emptyUsername ) {
+		return $response;
+	}
 
 	$response['status'] = 2;
 	return $response;
