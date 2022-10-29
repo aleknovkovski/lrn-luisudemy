9513816c613a69a4300e1b2a3735a9221cc154bc
Alek Novkovski <alek@greatcode2.com>
2022-10-29 18:21:52 +0200

    Use dynamic variables in markup for name and class

--- wp-content/plugins/udemy-plus/includes/blocks/header-tools.php
+++ wp-content/plugins/udemy-plus/includes/blocks/header-tools.php
@@ -10,12 +10,12 @@ function up_header_tools_render_cb($atts) {
 
 	<?php if($atts['showAuth']) { ?>
         <div class="wp-block-udemy-plus-header-tools">
-            <a class="signin-link open-modal" href="#">
+            <a class="signin-link <?php echo $openClass; ?>" href="#">
                 <div class="signin-icon">
                     <i class="bi bi-person-circle"></i>
                 </div>
                 <div class="signin-text">
-                    <small>Hello, Sign in</small>
+                    <small>Hello, <?php echo $name; ?></small>
                     My Account
                 </div>
             </a>
