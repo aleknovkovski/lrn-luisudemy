231a4fbde988a2f296e079c44ac8bb5d7613fa70
Alek Novkovski <alek@greatcode2.com>
2022-10-29 15:24:09 +0200

    Check if user, pass given; if not return bad status

--- wp-content/plugins/udemy-plus/includes/rest-api/signin.php
+++ wp-content/plugins/udemy-plus/includes/rest-api/signin.php
@@ -4,6 +4,14 @@ function up_rest_api_signin_handler($request) {
 	$response = ['status' => 1];
 	$params = $request->get_json_params();
 
+	$valuesNotSet = !isset($params['user_login'], $params['password']);
+	$userLoginIsEmpty = empty($params['user_login']);
+	$passwordIsEmpty = empty($params['password']);
+
+	if($valuesNotSet || $userLoginIsEmpty || $passwordIsEmpty) {
+		return $response;
+	}
+
 	$response['status'] = 2;
 	return $response;
 }
