33dc8be58a8fb0c599d3f32d36619d51893d042a
Alek Novkovski <alek@greatcode2.com>
2022-10-30 22:42:04 +0100

    Add term metadata into database as 'more_info_url'

--- wp-content/plugins/udemy-plus/includes/admin/save-cuisine.php
+++ wp-content/plugins/udemy-plus/includes/admin/save-cuisine.php
@@ -1,7 +1,12 @@
 <?php
 
-function up_save_cuisine_meta() {
+function up_save_cuisine_meta($term_id) {
 	if(!isset($_POST['up_more_info_url'])) {
 		return;
 	}
+	add_term_meta(
+		$term_id,
+		'more_info_url',
+		sanitize_url($_POST['up_more_info_url'])
+	);
 }
