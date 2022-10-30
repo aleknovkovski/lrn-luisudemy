71bf1b70e520afb8e8c006fa0675046f912f4b65
Alek Novkovski <alek@greatcode2.com>
2022-10-30 11:01:49 +0100

    Check WP Version before activating

--- wp-content/plugins/udemy-plus/includes/activate.php
+++ wp-content/plugins/udemy-plus/includes/activate.php
@@ -1,6 +1,13 @@
 <?php
 
 function up_activate_plugin(){
+	// 6.0 < 5.8
+	if(version_compare(get_bloginfo('version'), '5.8', '<')) {
+		wp_die(
+			__('You must update WordPress to use this plugin.', 'udemy-plus')
+		);
+	}
+
 	up_recipe_post_type();
 	flush_rewrite_rules();
 }
