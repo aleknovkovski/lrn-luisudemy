5ddeb4c8cae8750c906954bfa949c6f3a87e46ed
Alek Novkovski <alek@greatcode2.com>
2022-11-01 22:03:21 +0100

    Reformat cuisine-fields.php for readability

--- wp-content/plugins/udemy-plus/includes/admin/cuisine-fields.php
+++ wp-content/plugins/udemy-plus/includes/admin/cuisine-fields.php
@@ -6,12 +6,10 @@ function up_cuisine_add_form_fields() {
 		<label><?php _e('More Info URL', 'udemy-plus'); ?></label>
 		<input type="text" name="up_more_info_url">
 		<p>
-			<?php
-				_e(
+			<?php _e(
 					'A URL a user can click to learn more information about this cuisine',
 					'udemy-plus'
-				);
-			?>
+            ); ?>
 		</p>
 	</div>
 	<?php
