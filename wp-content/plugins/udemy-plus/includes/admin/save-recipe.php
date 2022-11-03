835dd248a8c1a48ecfb9fed9b6f66697eed217f3
Alek Novkovski <alek@greatcode2.com>
2022-11-03 20:48:04 +0100

    Set rating to 0 if doesn't exist from before

--- wp-content/plugins/udemy-plus/includes/admin/save-recipe.php
+++ wp-content/plugins/udemy-plus/includes/admin/save-recipe.php
@@ -4,4 +4,8 @@ function up_save_post_recipe ($post_id) {
     if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
 		return;
     }
+
+	$rating = get_post_meta($post_id, 'recipe_rating', true);
+	$rating = empty($rating) ? 0 : floatval($rating);
+	update_post_meta($post_id, 'recipe_rating', $rating);
 }
