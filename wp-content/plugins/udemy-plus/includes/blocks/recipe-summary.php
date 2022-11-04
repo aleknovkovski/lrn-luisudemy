ee91cea7ff20687cc595f6e4d20549c74db4178e
Alek Novkovski <alek@greatcode2.com>
2022-11-04 10:59:43 +0100

    Grab prepTime, cookTime and Course from $atts

--- wp-content/plugins/udemy-plus/includes/blocks/recipe-summary.php
+++ wp-content/plugins/udemy-plus/includes/blocks/recipe-summary.php
@@ -1,8 +1,13 @@
 <?php
 
-function up_recipe_summary_render_cb() {
+function up_recipe_summary_render_cb($atts) {
+    $prepTime = isset( $atts['prepTime']) ? esc_html($atts['prepTime']) : '';
+    $cookTime = isset( $atts['cookTime']) ? esc_html($atts['cookTime']) : '';
+    $course = isset( $atts['course']) ? esc_html($atts['course']) : '';
+
     ob_start();
     ?>
+
 
     <?php
     $output = ob_get_contents();
