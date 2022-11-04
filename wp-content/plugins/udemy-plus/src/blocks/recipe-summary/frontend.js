1588aebab54f4afe95ad90bc9bb6cd625a3b542c
Alek Novkovski <alek@greatcode2.com>
2022-11-04 14:37:59 +0100

    Render test message inside of block

--- wp-content/plugins/udemy-plus/src/blocks/recipe-summary/frontend.js
+++ wp-content/plugins/udemy-plus/src/blocks/recipe-summary/frontend.js
@@ -1,7 +1,16 @@
+import { render } from '@wordpress/element'
+
+function RecipeRating() {
+    return "Rendered by REACT"
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const block = document.querySelector("#recipe-rating");
     const postID = parseInt(block.dataset.postId);
     const avgRating = parseFloat(block.dataset.avgRating);
     const loggedIn = !!block.dataset.loggedIn;
     console.log(postID, avgRating, loggedIn);
+
+    render(
+        <RecipeRating/>, block);
 });
